db4ec25fd0ca202cd1faadeb7a2f60b2d938ac44
jacky0530-spec <jacky0530@gmail.com>
2026-09-17 15:10:41 +0800

    Move customer name migration to feature branch

--- db/customer-name.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE customers ADD COLUMN IF NOT EXISTS customer_name text;
-CREATE INDEX IF NOT EXISTS customers_customer_name_idx ON customers(customer_name) WHERE customer_name IS NOT NULL AND customer_name <> '';
